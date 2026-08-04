0bf79060c1befb6d61d269f36b3e55c0ff7d4c91
carmine <c.dimanso@studenti.unisa.it>
2026-08-04 10:54:44 +0200

    creazione del DB

---
+++ kft_DB/kft_DB.sql
@@ -0,0 +1,31 @@
+-- --------------------------------------------------------
+-- Host:                         127.0.0.1
+-- Versione server:              8.0.39 - MySQL Community Server - GPL
+-- S.O. server:                  Win64
+-- HeidiSQL Versione:            12.21.0.7344
+-- --------------------------------------------------------
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET NAMES utf8 */;
+/*!50503 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+-- Dump dei dati della tabella kft_db.admin: ~0 rows (circa)
+
+-- Dump dei dati della tabella kft_db.cliente: ~0 rows (circa)
+
+-- Dump dei dati della tabella kft_db.elementi: ~0 rows (circa)
+
+-- Dump dei dati della tabella kft_db.ordine: ~0 rows (circa)
+
+-- Dump dei dati della tabella kft_db.prodotto: ~0 rows (circa)
+
+/*!40103 SET TIME_ZONE=IFNULL(@OLD_TIME_ZONE, 'system') */;
+/*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
+/*!40014 SET FOREIGN_KEY_CHECKS=IFNULL(@OLD_FOREIGN_KEY_CHECKS, 1) */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40111 SET SQL_NOTES=IFNULL(@OLD_SQL_NOTES, 1) */;
