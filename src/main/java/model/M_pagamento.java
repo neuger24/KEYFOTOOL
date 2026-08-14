9efe2374596bd2986b4a7d43bd37c3f0756d5e5e
carmine <c.dimanso@studenti.unisa.it>
2026-08-14 21:16:26 +0200

    mod.: M_pagamento.java

---
+++ src/main/java/model/M_pagamento.java
@@ -0,0 +1,12 @@
+package model;
+
+public class M_pagamento {
+    private int id_metodo;
+    private int iD_Cliente;
+    private enum tipo{carta,PayPal};
+    private String username;
+    private String passward;
+    private String tnome_int;
+    private String cognome_int;
+    private
+}
