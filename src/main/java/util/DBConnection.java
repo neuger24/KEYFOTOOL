8767c0fdf0dcbcf9135f3a498cb98426db360b18
carmine <c.dimanso@studenti.unisa.it>
2026-09-06 10:53:42 +0200

    mod.: DBConnection.java

--- src/main/java/util/DBConnection.java
+++ src/main/java/util/DBConnection.java
@@ -1,6 +1,35 @@
 package util;
 
+import java.sql.Connection;
+import java.sql.SQLException;
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+import javax.sql.DataSource;
+
 public class DBConnection {
+
+    private static DataSource ds;
+
+    static {
+        try {
+            Context initCtx = new InitialContext();
+
+            Context envCtx = (Context) initCtx.lookup("java:comp/env");
+
+            ds = (DataSource) envCtx.lookup("jdbc/jdbc/kft_db");
+
+        } catch (NamingException e) {
+            System.err.println("Errore critico: impossibile trovare la risorsa DB nel context.xml. " + e.getMessage());
+        }
+    }
+    public static Connection getConnection() throws SQLException {
+        if (ds == null) {
+            throw new SQLException("Il DataSource non è stato inizializzato. Controlla la sintassi del tuo context.xml e assicurati che sia nella cartella META-INF.");
+        }
+
+        return ds.getConnection();
+    }
 
 
 }
